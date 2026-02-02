1f6cdbb68b4241322b39fb28b19af736338b9c12
Sumaima Saeed <k250093@nu.edu.pk>
2026-02-02 21:15:52 +0500

    employee.h

--- employee.h
+++ employee.h
@@ -1,21 +1,54 @@
-#ifndef EMPLOYEE_H
-#define EMPLOYEE_H
-
 #include <iostream>
 using namespace std;
 
 class Employee {
 private:
-    string employeeName;
-    double monthlySalary;
+    string Name;
+    double Salary;
     double taxPercentage;
 
 public:
-    void get_data();
-    double salary_after_tax();
-    void update_tax_percentage(double newTax);
-    void display();
+    void get_data() {
+        cout << "Enter employee name: ";
+        getline(cin, Name);
+
+        cout << "Enter monthly salary: ";
+        cin >> Salary;
+
+        cout << "Enter tax percentage: ";
+        cin >> taxPercentage;
+    }
+
+    double salary_after_tax() {
+        double taxAmount = (Salary * taxPercentage) / 100;
+        double remainingSalary = Salary - taxAmount;
+        return remainingSalary;
+    }
+
+    void update_tax_percentage() {
+        cout << "\nEnter new tax percentage: ";
+        cin >> taxPercentage;
+
+        double updatedSalary = salary_after_tax();
+        cout << "Updated salary after tax: Rs. "
+             << updatedSalary << endl;
+    }
+
+    void display() {
+        cout << "\nEmployee Name: " << Name << endl;
+        cout << "Original Salary: Rs. " << Salary << endl;
+        cout << "Tax Percentage: " << taxPercentage << "%\n";
+        cout << "Salary After Tax: Rs. "
+             << salary_after_tax() << endl;
+    }
 };
 
-#endif
+int main() {
+    Employee emp1;
 
+    emp1.get_data();
+    emp1.display();
+    emp1.update_tax_percentage();
+
+    return 0;
+}
